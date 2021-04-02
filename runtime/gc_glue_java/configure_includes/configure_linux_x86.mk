6d200f6aa1c05acbcde15dbdb96313723ffe25ca
Aleksandar Micic <amicic@ca.ibm.com>
2021-04-02 15:45:01 -0400

    disable CS build flag

--- runtime/gc_glue_java/configure_includes/configure_linux_x86.mk
+++ runtime/gc_glue_java/configure_includes/configure_linux_x86.mk
@@ -37,7 +37,6 @@ ifeq (linux_x86-64_cmprssptrs, $(SPEC))
 		--enable-OMR_ENV_LITTLE_ENDIAN \
 		--enable-OMR_GC_IDLE_HEAP_MANAGER \
 		--enable-OMR_GC_TLH_PREFETCH_FTA \
-		--enable-OMR_GC_CONCURRENT_SCAVENGER \
 		--enable-OMR_PORT_CAN_RESERVE_SPECIFIC_ADDRESS \
 		--enable-OMR_PORT_NUMA_SUPPORT \
 		OMR_GC_POINTER_MODE=compressed
@@ -67,7 +66,6 @@ ifeq (linux_x86-64, $(SPEC))
 		--enable-OMR_ENV_LITTLE_ENDIAN \
 		--enable-OMR_GC_IDLE_HEAP_MANAGER \
 		--enable-OMR_GC_TLH_PREFETCH_FTA \
-		--enable-OMR_GC_CONCURRENT_SCAVENGER \
 		--enable-OMR_PORT_CAN_RESERVE_SPECIFIC_ADDRESS \
 		--enable-OMR_PORT_NUMA_SUPPORT \
 		OMR_GC_POINTER_MODE=full
